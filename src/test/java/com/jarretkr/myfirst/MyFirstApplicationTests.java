0a017999e4f0eed238d6a17b2589b1b84589ff33
Kymani Jarrett <krj.kymani13@gmail.com>
2026-09-01 14:59:55 -0400

    Initial commit

---
+++ src/test/java/com/jarretkr/myfirst/MyFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarretkr.myfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
